4aaad81ba5d2238644fcb4d33161777431b6fca1
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-23 14:16:39 -0500

    edit app code so I think most of the methods work now

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,56 @@
 import { useState, useEffect } from 'react';
-
 import './App.scss';
-
-// Let's talk about using index.js and some other name in the component folder.
-// There's pros and cons for each way of doing this...
-// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
-// Why is this source of truth beneficial when spread across a global organization?
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 import axios from 'axios';
 
-
 function App() {
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const callApi = (requestParams) => {
-    // setLoading(true);
-    // mock output
-    //   const data = {
-    //     count: 2,
-    //     results: [
-    //       { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-    //       { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-    //     ],
-    //   };
-    //   setData(data);
-    //   setRequestParams(requestParams);
-    //   setLoading(false);
-    // }
-    // try {
-    //   let response = null;
-    //   if (requestParams.method === 'GET') {
-    //     response = await axios.get(requestParams.url);
-    //     setData(response.data);
-    //   } else if (requestParams.method === 'DELETE') {
-    //     response = await axios.delete(requestParams.url);
-    //     setData(response.data);
-    //   } else if (requestParams.method === 'PUT') {
-    //     response = await axios.put(requestParams.url, requestParams.body);
-    //     setData(response.data);
-    //   } else if (requestParams.method === 'POST') {
-    //     response = await axios.post(requestParams.url, requestParams.body);
-    //     setData(response.data);
-    //   } else {
-    //     throw new Error('Invalid request method.');
-    //   }
-    // } catch (error) {
-    //   console.error('API Error:', error);
-    // }
-    // setLoading(false);
-    setLoading(true); // gives a loading message
-    setTimeout(() => {
-    setRequestParams(requestParams);
-    setLoading(false);
-    }, 500);
-  }
-
   useEffect(() => {
-    try {
-      const getData = async () => {
+    const fetchData = async () => {
+      try {
         if (requestParams.url && requestParams.method) {
           setLoading(true);
-          console.log(requestParams);
-          let response = await axios(requestParams);
-          let results = response.data
-          setData(results);
+          let response;
+
+          switch (requestParams.method) {
+            case 'GET':
+              response = await axios.get(requestParams.url);
+              break;
+            case 'POST':
+              response = await axios.post(requestParams.url, requestParams.json);
+              break;
+            case 'PUT':
+              response = await axios.put(requestParams.url, requestParams.json);
+              break;
+            case 'DELETE':
+              response = await axios.delete(requestParams.url);
+              break;
+            default:
+              throw new Error('Invalid request method.');
+          }
+
+          setData(response.data);
           setLoading(false);
         }
+      } catch (err) {
+        console.error('API Error:', err);
+        setLoading(false);
       }
-      getData();
-    } catch (err) {
-      setData('No data available');
-    }
+    };
+
+    fetchData();
   }, [requestParams]);
 
-
-
+  const callApi = (requestParams) => {
+    setLoading(true);
+    setRequestParams(requestParams);
+  };
 
   return (
     <>
@@ -98,3 +69,4 @@ function App() {
 }
 
 export default App;
+
